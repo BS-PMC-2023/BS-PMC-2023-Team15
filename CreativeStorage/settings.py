a98da4576e2c8490e71058deaea2d47466eff228
Dori Fourer <dori.fourer654@gmail.com>
2023-04-02 17:36:38 +0300

    test

--- CreativeStorage/settings.py
+++ CreativeStorage/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+    'apps.item',
+    'apps.pages',
+
 ]
 
 MIDDLEWARE = [
