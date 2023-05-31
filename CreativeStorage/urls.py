8ea40f59df374e2be96e70362e446b028bb2f71c
Dori Fourer <dori.fourer654@gmail.com>
2023-05-31 11:56:26 +0300

    fix for test to run

--- CreativeStorage/urls.py
+++ CreativeStorage/urls.py
@@ -42,8 +42,6 @@ urlpatterns = [
     path('profile/view', main.views.profile_view, name='profile'),
     path('policy', main.views.policy_view, name='policy'),
     path('addstudents', main.views.addstudents, name='addstudents'),
-    path('pass/<item>', main.views.pass_item_view, name='pass'),
-    path('pass/<item>/<student>', main.views.pass_item_to_student, name='pass_item_to_student'),
     # path('contact', apps.pages.views.contact_view, name='contact'),
 ]
 
